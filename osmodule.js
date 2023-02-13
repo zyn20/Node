995cc06c92ec8f44b4ce19761cf766bfa5fcac1e
Zain Ul Abideen <mzainrajput6868@gmail.com>
2023-02-13 19:54:01 +0500

    update node

--- osmodule.js
+++ osmodule.js
@@ -1,18 +1,20 @@
-const os = require('os')
+const os = require("os");
 
-//suppose i want to the info of the current user 
-const user = os.userInfo()
-console.log(user)
+//suppose i want to the info of the current user
+const user = os.userInfo();
+console.log(user);
 
 // suppose we wan to take of the uptime of the system
 
-console.log(`The Uptime of my system is ${os.uptime()}`)
+console.log(`The Uptime of my system is ${os.uptime()}`);
 
-const osinfo =
-{
-    name:os.type(),
-    release:os.release(),
-    memory:os.totalmem(),
-    freemem:os.freemem()
-}
-console.log(osinfo)
+const osinfo = {
+  name: os.type(),
+  release: os.release(),
+  memory: os.totalmem(),
+  freemem: os.freemem(),
+  other: os.hostname(),
+  oth: os.machine(),
+  m: os.platform(),
+};
+console.log(osinfo);
