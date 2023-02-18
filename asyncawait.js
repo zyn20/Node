dbaaae407f2066213f620ec2a697bdc5e2864571
Zain Ul Abideen <mzainrajput6868@gmail.com>
2023-02-18 16:44:27 +0500

    Update asyncawait.js

---
+++ asyncawait.js
@@ -0,0 +1,14 @@
+ const axiosRequest = require('axios')
+ async function getActivity()
+ {
+    try {
+        let response =  await axiosRequest.get('https://www.boredapi.com/api/activity');
+console.log(response.data.activity);
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+getActivity();
