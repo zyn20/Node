66270d64918b4a3d3b8eec99abbe49903463fe9b
Zain Ul Abideen <mzainrajput6868@gmail.com>
2023-02-18 16:08:57 +0500

    axios Libaray

--- promises.js
+++ promises.js
@@ -1,16 +1,27 @@
-//syntax
-const whereismycofee = function(orderid)
-{
-    return new Promise ((resolve,reject) =>{
-      Cofee.checkApi(orderid, (error,coffestarus) =>
-      {
-        if(error)
-        {
-            console.log("Error");
-        }
-        else{
-            return coffestarus;
-        }
-      })
-    })
-}
+// //syntax
+// const whereismycofee = function(orderid)
+// {
+//     return new Promise ((resolve,reject) =>{
+//       Cofee.checkApi(orderid, (error,coffestarus) =>
+//       {
+//         if(error)
+//         {
+//             console.log("Error");
+//         }
+//         else{
+//             return coffestarus;
+//         }
+//       })
+//     })
+// }
+const { default: axios } = require('axios');
+//working with axious client library Axios is a promise based HTTP client for the browser and Node.js
+
+const axiosrequest = require('axios')
+axiosrequest.get('https://www.boredapi.com/api/activity')
+.then(response =>{
+    console.log(`you could ${response.data.activity}` )
+})
+.catch(error =>{
+    console.log(`Error ${error}` )
+})
